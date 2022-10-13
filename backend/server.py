c7a66d95684b243e8d137eaee5d2dc5d8dbd80bb
codingw200lbs <67208922+codingw200lbs@users.noreply.github.com>
2022-10-13 15:45:22 -0500

    initial commit

---
+++ backend/server.py
@@ -0,0 +1,35 @@
+
+# Import flask and datetime module for showing date and time
+from flask import Flask
+import datetime
+from flask_cors import CORS
+  
+x = datetime.datetime.now()
+  
+# Initializing flask app
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return "<p>Welcome To Batoshi!</p>"
+  
+# Route for seeing a data
+@app.route("/data")
+def get_time():
+  
+    # Returning an api for showing in  reactjs
+    return {
+        "ID" : "001",
+        "Model":"Antminer S19 Pro", 
+        "HashRate":"110T",
+        "Power":"3450W", 
+        "InstalledDate":"09/01/2022"
+        }
+  
+'''
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
+'''
+
